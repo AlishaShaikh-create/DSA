ae0c87e52510273303b5ba0ba3d2e1109e1722ad
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-08-19 15:26:20 +0530

    gas stations

---
+++ 04-Binary_Search/02-BS_OnAns/11-gas_stations.py
@@ -0,0 +1,34 @@
+import math
+class Solution:
+    def gas_station(self,nums,k):
+        low = 0.0 
+        high = 0
+        for i in range(1,len(nums)):
+            distance = nums[i]-nums[i-1]
+            if distance > high :
+                high = distance
+
+        for _ in range(100):
+            mid = (low + high)/2
+            if self.can_place(nums,k,mid):
+                high = mid 
+            else :
+                low = mid 
+        return  high                  
+
+            
+    
+    def can_place(self,nums,k,mid):
+        station = 0
+        for i in range(1, len(nums)):
+            gap = nums[i] - nums[i-1]
+            station += math.ceil(gap/mid)-1
+
+            if station > k:
+                return False
+        return True    
+
+arr = [0, 10]
+k = 1
+sol = Solution()
+print(sol.gas_station(arr,k))        
